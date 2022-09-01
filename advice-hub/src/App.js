cf62d0b8554461594450c9da9b1d531527f0cb05
Hawkeye-H <HHrones@magmutual.com>
2022-09-01 14:44:21 -0400

    CRA setup
    Init CRA, webpack, eslint, prettier, etc. Styling of components looks a little off, but that may be a component issue, not an installation issue

---
+++ advice-hub/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "@carbon/react"
+import React from "react"
+
+function App() {
+  return (
+    <div>
+      <Button size="2xl">Button</Button>
+    </div>
+  )
+}
+
+export default App
