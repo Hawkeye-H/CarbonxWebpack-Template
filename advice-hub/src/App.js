656aed1faff3405807fbad0b62cd90174565bf76
Hawkeye-H <HHrones@magmutual.com>
2022-09-07 11:41:20 -0400

    Update App.js

--- advice-hub/src/App.js
+++ advice-hub/src/App.js
@@ -1,14 +1,10 @@
-import { Button, Theme } from "@carbon/react"
+import { Button } from "@carbon/react"
 import React from "react"
 
 function App() {
   return (
     <div>
-      <Theme theme="white">
-        <Button size="2xl" kind="primary">
-          Button
-        </Button>
-      </Theme>
+      <Button>Button</Button>
     </div>
   )
 }
